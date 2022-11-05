43acec67cee9a0e5bf08191de4622962dfd6caa6
Kevin Souza <kevinsouza@Kevins-MacBook-Pro.local>
2022-11-04 22:33:56 -0400

    homepage done

--- biopage/app.js
+++ biopage/app.js
@@ -4,21 +4,17 @@
 
 
 $(() => {
-  const $headerBP = $('<div>').addClass('header').appendTo('body');
+  const $headerBP = $('<div>').addClass('header');
   const $h1BP = $('<h1>').text('Background').appendTo($headerBP).css('text-align', 'center');
   // first pic that will also be modal
-  const $topImg = $('<div>').appendTo('body')
+  const $topSec = $('<div>')
   const $img1 = $('<img>').attr('src', '').appendTo($topImg);
   const $text1 = $('<p>').text('').appendTo($topImg)
-
+  $('body').prepend($topSec)
+  $('body').prepend($headerBP)
 
   // footer
   const $footer = $('<div>').addClass('footer').appendTo('body');
-  const $homePage = $('<button>').text('HOME').appendTo($footer);
-  const $bioPage = $('<button>').text('BIO').appendTo($footer);
-  const $resumePage = $('<button>').text('RESUME').appendTo($footer);
-  const $projectPage = $('<button>').text('PROJECTS').appendTo($footer);
-  const $gitHub = $('<button>').text('GITHUB').appendTo($footer);
-  const $linkdPage = $('<button>').text('LINKEDIN').appendTo($footer);
+
 
 })
