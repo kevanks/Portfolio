274bfbbbdcd5c5cc28e1543108aed2946f4ceaa5
Kevin Souza <kevinsouza@Kevins-MacBook-Pro.local>
2022-11-05 23:54:25 -0400

    only need styling now everything else done

--- biopage/app.js
+++ biopage/app.js
@@ -12,36 +12,53 @@ $(() => {
   const $image1 = $('<div>').addClass('image').appendTo($topSec);
   const $img1 = $('<img>').attr('src', 'file:///Users/kevinsouza/Downloads/IMG_4134%202.jpg').appendTo($image1);
   const $bttn1 = $('<button>').addClass('openModal').text('Expand').appendTo($topSec);
-  const $modal1 = $('<div>').addClass('modal').appendTo($bttn1);
-  const $modalImg = $('<img>').addClass('modalImg').attr('src', 'file:///Users/kevinsouza/Downloads/IMG_4129.jpg').appendTo($modal1);
-  const $closebttn = $('<div>').addClass('closeModal').html('<a href="#">Close</a>').appendTo($modal1)
+  const $modal1 = $('<div>').addClass('modal').css('display', 'none').appendTo($topSec);
+  const $modalImg = $('<img>').addClass('modalImg').css('display', 'none').attr('src', 'file:///Users/kevinsouza/Downloads/IMG_4139.JPG').appendTo($modal1);
+  const $closebttn = $('<div>').addClass('closeModal').css('display', 'none').html('<a href="#">Close</a>').appendTo($modal1);
   const $text1 = $('<p>').text('Kevin Souza was born in Brockton MA in 2000 to two Brazilian immigrants. Kevin Souza was born in Brockton MA in 2000 to two Brazilian immigrants. Kevin Souza was born in Brockton MA in 2000 to two Brazilian immigrants. Kevin Souza was born in Brockton MA in 2000 to two Brazilian immigrants. Kevin Souza was born in Brockton MA in 2000 to two Brazilian immigrants. Kevin Souza was born in Brockton MA in 2000 to two Brazilian immigrants. Kevin Souza was born in Brockton MA in 2000 to two Brazilian immigrants. Kevin Souza was born in Brockton MA in 2000 to two Brazilian immigrants. Kevin Souza was born in Brockton MA in 2000 to two Brazilian immigrants. Kevin Souza was born in Brockton MA in 2000 to two Brazilian immigrants.').appendTo($topSec);
   // sec pic that will be modal and bottom half of page
   const $bttmSec = $('<container>').addClass('bottomSection');
   const $image2 = $('<div>').addClass('image').appendTo($bttmSec)
   const $img2 = $('<img>').attr('src', 'file:///Users/kevinsouza/Downloads/IMG_4131.JPG').appendTo($image2);
+  const $bttn2 = $('<button>').addClass('openModal').text('Expand').appendTo($bttmSec);
+  const $modal2 = $('<div>').addClass('modal').css('display', 'none').appendTo($bttmSec);
+  const $modalImg2 = $('<img>').addClass('modalImg').css('display', 'none').attr('src', 'file:///Users/kevinsouza/Downloads/IMG_4130%202.jpg').appendTo($modal2);
+  const $closebttn2 = $('<div>').addClass('closeModal').css('display', 'none').html('<a href="#">Close</a>').appendTo($modal2);
   const $text2 = $('<p>').text('Kevin Souza was born in Brockton MA in 2000 to two Brazilian immigrants. Kevin Souza was born in Brockton MA in 2000 to two Brazilian immigrants. Kevin Souza was born in Brockton MA in 2000 to two Brazilian immigrants. Kevin Souza was born in Brockton MA in 2000 to two Brazilian immigrants. Kevin Souza was born in Brockton MA in 2000 to two Brazilian immigrants. Kevin Souza was born in Brockton MA in 2000 to two Brazilian immigrants. Kevin Souza was born in Brockton MA in 2000 to two Brazilian immigrants. Kevin Souza was born in Brockton MA in 2000 to two Brazilian immigrants. Kevin Souza was born in Brockton MA in 2000 to two Brazilian immigrants. Kevin Souza was born in Brockton MA in 2000 to two Brazilian immigrants.').appendTo($bttmSec);
   $('body').prepend($bttmSec);
   $('body').prepend($topSec);
   $('body').prepend($headerBP);
 
-  // modal
+  // modal 1
   const openModal = () => {
     $modal1.css('display', 'block')
     $modalImg.css('display', 'block')
+    $closebttn.css('display', 'block')
   }
-
-  $bttn1.on('click', openModal)
 
   const closeModal = () => {
     $modal1.css('display', 'none')
     $modalImg.css('display', 'none')
+    $closebttn.css('display', 'none')
   }
 
+  $bttn1.on('click', openModal)
   $closebttn.on('click', closeModal)
 
+  // modal 2
+  const openModal2 = () => {
+    $modal2.css('display', 'block')
+    $modalImg2.css('display', 'block')
+    $closebttn2.css('display', 'block')
+  }
 
+  const closeModal2 = () => {
+    $modal2.css('display', 'none')
+    $modalImg2.css('display', 'none')
+    $closebttn2.css('display', 'none')
+  }
 
-
+  $bttn2.on('click', openModal2)
+  $closebttn2.on('click', closeModal2)
 
 })
--- projectpage/app.js
+++ projectpage/app.js
@@ -6,22 +6,22 @@ $(() => {
   const $projects = $('<container>').addClass('projects');
   // project 1
   const $pro1 = $('<div>').appendTo($projects);
-  const $pro1Img = $('<img>').attr('src', ' ').appendTo($pro1);
+  const $pro1Img = $('<img>').attr('src', 'https://www.generationsforpeace.org/wp-content/uploads/2018/03/empty.jpg').appendTo($pro1);
   const $pro1Name = $('<h2>').text('Project Name').appendTo($pro1);
   const $pro1Des = $('<p>').text('Quick description of project').appendTo($pro1);
   // project 2
   const $pro2 = $('<div>').appendTo($projects);
-  const $pro2Img = $('<img>').attr('src', ' ').appendTo($pro2);
+  const $pro2Img = $('<img>').attr('src', 'https://www.generationsforpeace.org/wp-content/uploads/2018/03/empty.jpg').appendTo($pro2);
   const $pro2Name = $('<h2>').text('Project Name').appendTo($pro2);
   const $pro2Des = $('<p>').text('Quick description of project').appendTo($pro2);
   // project 3
   const $pro3 = $('<div>').appendTo($projects);
-  const $pro3Img = $('<img>').attr('src', ' ').appendTo($pro3);
+  const $pro3Img = $('<img>').attr('src', 'https://www.generationsforpeace.org/wp-content/uploads/2018/03/empty.jpg').appendTo($pro3);
   const $pro3Name = $('<h2>').text('Project Name').appendTo($pro3);
   const $pro3Des = $('<p>').text('Quick description of project').appendTo($pro3);
   // project 4
   const $pro4 = $('<div>').appendTo($projects);
-  const $pro4Img = $('<img>').attr('src', ' ').appendTo($pro4);
+  const $pro4Img = $('<img>').attr('src', 'https://www.generationsforpeace.org/wp-content/uploads/2018/03/empty.jpg').appendTo($pro4);
   const $pro4Name = $('<h2>').text('Project Name').appendTo($pro4);
   const $pro4Des = $('<p>').text('Quick description of project').appendTo($pro4);
 
