3f4a68714658925f59f42148cc371449a025f0c5
Kevin Souza <kevinsouza@Kevins-MacBook-Pro.local>
2022-11-06 23:23:24 -0500

    resume page and project page and homepage done just need to style bio lil more

--- projectpage/app.js
+++ projectpage/app.js
@@ -5,25 +5,29 @@ $(() => {
   // container where all future projects will go
   const $projects = $('<container>').addClass('projects');
   // project 1
-  const $pro1 = $('<div>').appendTo($projects);
-  const $pro1Img = $('<img>').attr('src', 'https://www.generationsforpeace.org/wp-content/uploads/2018/03/empty.jpg').appendTo($pro1);
-  const $pro1Name = $('<h2>').text('Project Name').appendTo($pro1);
-  const $pro1Des = $('<p>').text('Quick description of project').appendTo($pro1);
+  const $pro1 = $('<div>').addClass('project').appendTo($projects);
+  const $pro1Img = $('<img>').attr('src', 'https://www.gamblingsites.org/app/uploads/2020/04/blackjack-cards-image-new-1.jpg').appendTo($pro1);
+  const $pro1Name = $('<h2>').text('Blackjack').appendTo($pro1);
+  const $pro1Des = $('<p>').text('Virtual blackjack where the dealer always gets 21.').appendTo($pro1);
+  const $pro1Link = $('<div>').addClass('link').html('<a href="#">Blackjack Game</a>').appendTo($pro1);
   // project 2
-  const $pro2 = $('<div>').appendTo($projects);
-  const $pro2Img = $('<img>').attr('src', 'https://www.generationsforpeace.org/wp-content/uploads/2018/03/empty.jpg').appendTo($pro2);
-  const $pro2Name = $('<h2>').text('Project Name').appendTo($pro2);
-  const $pro2Des = $('<p>').text('Quick description of project').appendTo($pro2);
+  const $pro2 = $('<div>').addClass('project').appendTo($projects);
+  const $pro2Img = $('<img>').attr('src', 'https://cdn2.vectorstock.com/i/thumb-large/74/21/black-chess-bishop-piece-on-white-background-vector-35207421.jpg').appendTo($pro2);
+  const $pro2Name = $('<h2>').text('Chess Game').appendTo($pro2);
+  const $pro2Des = $('<p>').text('Virtual game of chess where you always win so you can feel smart.').appendTo($pro2);
+  const $pro2Link = $('<div>').addClass('link').html('<a href="#">Chess Game</a>').appendTo($pro2);
   // project 3
-  const $pro3 = $('<div>').appendTo($projects);
-  const $pro3Img = $('<img>').attr('src', 'https://www.generationsforpeace.org/wp-content/uploads/2018/03/empty.jpg').appendTo($pro3);
-  const $pro3Name = $('<h2>').text('Project Name').appendTo($pro3);
-  const $pro3Des = $('<p>').text('Quick description of project').appendTo($pro3);
+  const $pro3 = $('<div>').addClass('project').appendTo($projects);
+  const $pro3Img = $('<img>').attr('src', 'https://pngimg.com/uploads/facebook_logos/small/facebook_logos_PNG19761.png').appendTo($pro3);
+  const $pro3Name = $('<h2>').text('FaceBook').appendTo($pro3);
+  const $pro3Des = $('<p>').text('Yes, I created facebook. Mark stole it from me. Not cool Mark.').appendTo($pro3);
+  const $pro3Link = $('<div>').addClass('link').html('<a href="https://www.facebook.com/">FaceBook</a>').appendTo($pro3);
   // project 4
-  const $pro4 = $('<div>').appendTo($projects);
-  const $pro4Img = $('<img>').attr('src', 'https://www.generationsforpeace.org/wp-content/uploads/2018/03/empty.jpg').appendTo($pro4);
-  const $pro4Name = $('<h2>').text('Project Name').appendTo($pro4);
-  const $pro4Des = $('<p>').text('Quick description of project').appendTo($pro4);
+  const $pro4 = $('<div>').addClass('project').appendTo($projects);
+  const $pro4Img = $('<img>').attr('src', 'https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,w_221,h_221/v1618459302/assets/8e/cbcd2e-e7f1-4bd1-ae4e-5343b99ff22f/original/Driver-App-logo.png').appendTo($pro4);
+  const $pro4Name = $('<h2>').text('Uber').appendTo($pro4);
+  const $pro4Des = $('<p>').text('Yes I also created Uber in my spare time.').appendTo($pro4);
+  const $pro4Link = $('<div>').addClass('link').html('<a href="https://www.uber.com/us/en/s/d/kochab/?ad_id=619191988920&adg_id=139968575959&campaign_id=18209768604&cre=619191988920&dev=c&dev_m=&fi_id=&gclid=CjwKCAjwtp2bBhAGEiwAOZZTuAfT7yTLCzN1ZQFG_7o1tEYnQkL1O1hu7VGxeA7vJF-blGsDnG1moxoCPCoQAvD_BwE&gclsrc=aw.ds&kw=uber&kwid=kwd-12633382&match=b&net=g&placement=&tar=&utm_campaign=CM2200161-search-google-brand_1_-99_US-National_o-d_web_acq_cpc_en_T3_Generic_BM_uber_kwd-12633382_619191988920_139968575959_b_c&utm_source=AdWords_Brand">Uber</a>').appendTo($pro4)
 
   $('body').prepend($projects);
   $('body').prepend($headerPP);
--- resumepage/app.js
+++ resumepage/app.js
@@ -7,41 +7,40 @@ $(() => {
   const $backBttn = $('<button>').addClass('back-button').text('<').appendTo($resume);
   const $carousel = $('<div>').addClass('skill-exp-edc').appendTo($resume);
   // experience
-  const $experience = $('<div>').css('display', 'block').appendTo($carousel);
+  const $experience = $('<div>').addClass('exp').css('display', 'block').appendTo($carousel);
   const $expH2 = $('<h2>').text('Experience').css('text-align', 'center').appendTo($experience);
   const $expUl = $('<ul>').appendTo($experience);
-  const $expLi1 = $('<li>').text("Dunkin' Donuts cir. May 2016 - Sept 2016").appendTo($experience);
-  const $expLi2 = $('<li>').text("Tommy's Pub & Grill cir. April 2017 - Sept 2018").appendTo($experience);
-  const $expLi3 = $('<li>').text("Aeropostale cir. June 2017 - Dec 2017").appendTo($experience);
-  const $expLi4 = $('<li>').text("Amazon cir. May 2019 - Sept 2019").appendTo($experience);
-  const $expLi5 = $('<li>').text("Target cir. May 2020 - June 2021").appendTo($experience);
-  const $expLi6 = $('<li>').text("Dominos cir. Oct 2020 - April 2021").appendTo($experience);
-  const $expLi7 = $('<li>').text("InstaCart cir. August 2021 - Present").appendTo($experience);
+  const $expLi1 = $('<li>').text("Dunkin' Donuts cir. May 2016 - Sept 2016").css('list-style-type', 'none').appendTo($experience);
+  const $expLi2 = $('<li>').text("Tommy's Pub & Grill cir. April 2017 - Sept 2018").css('list-style-type', 'none').appendTo($experience);
+  const $expLi3 = $('<li>').text("Aeropostale cir. June 2017 - Dec 2017").css('list-style-type', 'none').appendTo($experience);
+  const $expLi4 = $('<li>').text("Amazon cir. May 2019 - Sept 2019").css('list-style-type', 'none').appendTo($experience);
+  const $expLi5 = $('<li>').text("Target cir. May 2020 - June 2021").css('list-style-type', 'none').appendTo($experience);
+  const $expLi6 = $('<li>').text("Dominos cir. Oct 2020 - April 2021").css('list-style-type', 'none').appendTo($experience);
+  const $expLi7 = $('<li>').text("InstaCart cir. August 2021 - Present").css('list-style-type', 'none').appendTo($experience);
   // skills
-  const $skills = $('<div>').css('display', 'none').appendTo($carousel);
+  const $skills = $('<div>').addClass('skill').css('display', 'none').appendTo($carousel);
   const $skillH2 = $('<h2>').text('Skills').css('text-align', 'center').appendTo($skills);
   const $skillsUl = $('<ul>').appendTo($skills);
-  const $skillLi1 = $('<li>').text("Time Mangement").appendTo($skills);
-  const $skillLi2 = $('<li>').text("Customer Service").appendTo($skills);
-  const $skillLi3 = $('<li>').text("Critical Thinking").appendTo($skills);
-  const $skillLi4 = $('<li>').text("JavaScript").appendTo($skills);
-  const $skillLi5 = $('<li>').text("JQuery").appendTo($skills);
-  const $skillLi6 = $('<li>').text("HTML").appendTo($skills);
-  const $skillLi7 = $('<li>').text("CSS").appendTo($skills);
-  const $skillLi8 = $('<li>').text("GitHub").appendTo($skills);
-  const $skillLi9 = $('<li>').text("Fluent in Portuguese").appendTo($skills);
+  const $skillLi1 = $('<li>').text("Time Mangement").css('list-style-type', 'none').appendTo($skills);
+  const $skillLi2 = $('<li>').text("Customer Service").css('list-style-type', 'none').appendTo($skills);
+  const $skillLi3 = $('<li>').text("Critical Thinking").css('list-style-type', 'none').appendTo($skills);
+  const $skillLi4 = $('<li>').text("JavaScript").css('list-style-type', 'none').appendTo($skills);
+  const $skillLi5 = $('<li>').text("JQuery").css('list-style-type', 'none').appendTo($skills);
+  const $skillLi6 = $('<li>').text("HTML").css('list-style-type', 'none').appendTo($skills);
+  const $skillLi7 = $('<li>').text("CSS").css('list-style-type', 'none').appendTo($skills);
+  const $skillLi8 = $('<li>').text("GitHub").css('list-style-type', 'none').appendTo($skills);
   // education
-  const $education = $('<div>').css('display', 'none').appendTo($carousel);
+  const $education = $('<div>').addClass('edc').css('display', 'none').appendTo($carousel);
   const $eduH2 = $('<h2>').text('Education').css('text-align', 'center').appendTo($education);
   const $eduUl = $('<ul>').appendTo($education);
-  const $eduLi1 = $('<li>').text("Brockton High School - Diploma").appendTo($education);
-  const $eduLi2 = $('<li>').text("UMass Amherst - Math with a Concentration in Computing - Unfinished").appendTo($education);
-  const $eduLi3 = $('<li>').text("General Assembly - Software Engineering Immersive Program - Present").appendTo($education);
+  const $eduLi1 = $('<li>').text("Brockton High School - Diploma").css('list-style-type', 'none').appendTo($education);
+  const $eduLi2 = $('<li>').text("UMass Amherst - Math with a Concentration in Computing - Unfinished").css('list-style-type', 'none').appendTo($education);
+  const $eduLi3 = $('<li>').text("General Assembly - Software Engineering Immersive Program - Present").css('list-style-type', 'none').appendTo($education);
 
   const $nextBttn = $('<button>').addClass('next-button').text('>').appendTo($resume);
   // button that brings you to my full resume on a pdf
-  const $resumeBttn = $('<button>').addClass('resume-button').text('Full Resume');
-  $('body').prepend($resumeBttn);
+  const $fullResume = $('<div>').addClass('full-resume').html('<a href="https://docs.google.com/document/d/1gIp2tVqdERVrQOacBsQh0Dcl8OscRE2w/edit#">Full Resume</a>');
+  $('body').prepend($fullResume);
   $('body').prepend($resume);
   $('body').prepend($headerRP);
 
@@ -70,11 +69,6 @@ $(() => {
       currentImgIndex = numOfImg;
     }
     $('.skill-exp-edc').children().eq(currentImgIndex).css('display', 'block');
-  })
-
-  // resume button
-  $('.resume-button').on('click', () => {
-
   })
 
 })
